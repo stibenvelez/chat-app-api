7fd23b7b277d69c8ee3ab24ed5daabaea9a0bad3
stibenvelez <stibenvelez@hotmail.com>
2022-08-17 23:55:23 -0500

    last changes

--- index.js
+++ index.js
@@ -12,26 +12,34 @@ const io = new Server(server, {
     cors: {
         origin: process.env.FRONTEND_URL,
     },
-});
+}); 
 
 io.on("connection", (socket) => {
+    const id = socket.id;
     let name = socket.on("conected", (data) => {
         name = data;
         socket.broadcast.emit("messages", {
+            id,
             name,
-            message: `${name} has joined the chat`,
+            message: `${name} se ha unido al chat`,
+            type: "notification"
         });
     });
 
     socket.on("disconnect", () => {
         io.emit("messages", {
+            id,
             server: "Server",
-            message: `${name}has left the chat`,
+            message: `${name} ha dejado el chat`,
+            type: "notification"
         });
     });
 
     socket.on("message", (name, message) => {
-        io.emit("messages", { name, message });
+        io.emit("messages", { id, name, message, type: "message" });
+    });
+    socket.on("writing", (name) => {
+        socket.broadcast.emit("writing", { id, name, message: `${name} esta escribiendo`, type: "writing" });
     });
 });
 
